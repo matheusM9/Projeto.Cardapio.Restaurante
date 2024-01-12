840437b97cca31b8c3161747159cf4f83599cdca
matheusM9 <103466098+matheusM9@users.noreply.github.com>
2024-01-12 14:21:03 -0300

    Add files via upload

---
+++ CardapioFrango/script.js
@@ -0,0 +1,15 @@
+
+function time()
+{
+today=new Date();
+h=today.getHours();
+m=today.getMinutes();
+s=today.getSeconds();
+ document.getElementById('txt').innerHTML=h+":"+m+":"+s;
+setTimeout('time()',500);
+}
+
+
+
+  
+    
